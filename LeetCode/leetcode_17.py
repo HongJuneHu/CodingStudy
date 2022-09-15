d425de22e882cbe49fa08a4d52fcb6e3f41ff3ec
HongJuneHu <gjekd99@gmail.com>
2022-09-15 21:24:51 +0900

    2022-09-15

---
+++ LeetCode/leetcode_17.py
@@ -0,0 +1,16 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        
+        numbers = {2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4: ['g', 'h', 'i'], 5: ['j', 'k', 'l'], 6: ['m', 'n', 'o'], 
+                  7: ['p', 'q', 'r', 's'], 8: ['t', 'u', 'v'], 9: ['w', 'x', 'y', 'z']}
+        res = [""]
+
+        for i in digits:
+            tmp = []
+            for j in numbers[int(i)]:
+                for k in res:
+                    tmp.append(k + j)
+            res = tmp
+        return res
