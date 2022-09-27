216efb2215ea5973dbeb5692dda7e4fa6c217905
HongJuneHu <gjekd99@gmail.com>
2022-09-27 18:37:53 +0900

    2022-09-27

---
+++ BaekJoon/baekjoon_2439.py
@@ -0,0 +1,8 @@
+import sys
+a = int(sys.stdin.readline())
+for i in range(a):
+    for j in range(a-i-1):
+        print(" ", end="")
+    for k in range(i+1):
+        print("*", end="")
+    print()
